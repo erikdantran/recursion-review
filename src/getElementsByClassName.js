ad00b9cd1a89f076a15879adcdfd8c836317e516
Erik Dan Tran <erikdantran99@gmail.com>
2021-09-20 17:41:03 -0500

    Completed getElementByClassName and currently working on stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,47 @@
 //   return document.getElementsByClassName(className);
 // };
 
+
+// var htmlStrings = [
+//   '<div class="targetClassName"></div>',
+//   '<div class="otherClassName targetClassName"></div>',
+//   '<div><div class="targetClassName"></div></div>',
+//   '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
+//   '<div><div></div><div><div class="targetClassName"></div></div></div>',
+//   '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+//   '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+// ];
+
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function (className) {
+  //use document.body, element.childNodes, and element.classList
   // your code here
+
+  //if document.body does contain classname, then recall function and add result to result
+  //iterate through document.body, check if classname is inside classlist (use contain)
+  var result = [];
+  //variable result that is an array
+  var innerMethod = function (element) {
+    //create innerMethod function taking element as parameter
+    if (element.classList && element.classList.contains(className)) {
+      //if classlist exists and if the classlist contains classname
+      result.push(element);
+      //push the element into result
+    }
+    // console.log(result)
+    element.childNodes.forEach(innerMethod);
+    //iterate through each childnode in element using innerMethod
+  };
+  innerMethod(document.body);
+  // console.log(result)
+  // result.join()
+  return result;
+  // console.log(result)
 };
+
+
+
+
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,82 @@
 
 // but you don't so you're going to write it from scratch:
 
+// var stringifiableObjects = [
+//   9,
+//   null,
+//   true,
+//   false,
+//   'Hello world',
+//   [],
+//   [8],
+//   ['hi'],
+//   [8, 'hi'],
+//   [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+//   [8, [[], 3, 4]],
+//   [[[['foo']]]],
+//   {},
+//   {'a': 'apple'},
+//   {'foo': true, 'bar': false, 'baz': null},
+//   {'boolean, true': true, 'boolean, false': false, 'null': null },
+//   // basic nesting
+//   {'a': {'b': 'c'}},
+//   {'a': ['b', 'c']},
+//   [{'a': 'b'}, {'c': 'd'}],
+//   {'a': [], 'c': {}, 'b': true}
+// ];
+
+// // used for stringifyJSON spec
+// // hint: JSON does not allow you to stringify functions or
+// // undefined values, so you should skip those key/value pairs.
+// unstringifiableValues = [
+//   {
+//     'functions': function() {},
+//     'undefined': undefined
+//   }
+// ];
+
+
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  var string = '';
+  if (obj === null ) {
+    return 'null';
+  }
+  if (typeof obj === 'number' ||
+      typeof obj === 'boolean') {
+    string += obj.toString();
+  }
+  if (typeof obj === 'string') {
+    string += '"' + obj + '"';
+  }
+  if (typeof obj === 'object' && Array.isArray(obj)) {
+    string += '[';
+    // string += obj.forEach(element, stringifyJSON)
+    for (var i = 0; i < obj.length; i++) {
+      string += stringifyJSON(obj[i])
+      if (i !== obj.length-1) {
+        string += ',';
+      }
+    }
+    string += ']';
+  }
+
+  if (typeof obj === 'object' && !Array.isArray(obj)) {
+    string += '{';
+    for (var key in obj) {
+      string += stringifyJSON(key);
+      string += ':';
+      string += stringifyJSON(obj[key])
+    }
+    string += '}';
+  }
+
+
+  return string
 };
+
+
+
+
+// var stringifyJS
